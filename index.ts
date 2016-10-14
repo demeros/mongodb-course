2944f0c98b78a98ceffe0f56db6b59532cdb5f7a
Nazar Sakharenko <nazar@sakharenko.com>
2016-10-14 13:40:05 +0300

    views + connection to mongodb

--- index.ts
+++ index.ts
@@ -1,9 +1,41 @@
 'use strict';
 
 import * as Express from 'express';
-import * as Mongodb from 'mongodb';
+import * as Engines from 'consolidate';
+import {MongoClient} from 'mongodb';
 
-import { each } from 'lodash';
+import * as assert from 'assert';
+import {join} from 'path';
+
 
 const app = Express();
+
+app.engine('html', Engines.nunjucks);
+app.set('view_engine', 'html');
+app.set('views', join(__dirname, 'app', 'views'));
+
+MongoClient.connect('mongodb://localhost:27017/video', (error, db) => {
+    assert.equal(null, error);
+
+    console.log('MongoDB connected');
+
+    app.get('/', (req, res) => {
+        db.collection('movies')
+            .find({})
+            .toArray((error, movies) => {
+                res.render('movies', {movies});
+            });
+    });
+
+    app.use((req, res) => {
+        res.sendStatus(404);
+    });
+
+    const server = app.listen(3000, () => {
+        const port = server.address().port;
+        console.info('Express listening on port %s', port);
+    });
+
+});
+
 
